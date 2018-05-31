0b4c205da5e5246a6f2e15ddc19a4deb51508d1e
Gürkan Gür <gurkan.gur@innogames.com>
2018-05-31 16:59:18 +0200

    Parameterize the extension

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
         pipe = os.popen("if tmux ls > /dev/null 2>&1; then tmux ls; else echo none; fi")
+        terminal_binary = extension.preferences['terminal_binary']
+        console_parameters_attach = extension.preferences['console_parameters_attach']
+        console_parameters_new = extension.preferences['console_parameters_new']
         output = pipe.read()
         if output.splitlines()[0] != "none":
             for line in output.splitlines():
                 items.append(
                     ExtensionResultItem(
                         icon='images/tmux.png',
-                        name='Attach session %s' % line.split(" ")[0],
+                        name='Attach session %s' % line.split(' ')[0],
                         description=line,
-                        on_enter=RunScriptAction("gnome-terminal --tab -e \"tmux att -t %s\"" % line.split(" ")[0], None)
+                        on_enter=RunScriptAction(terminal_binary + ' ' + console_parameters_attach % line.split(' ')[0], None)
                     )
                 )
         else:
@@ -35,7 +38,7 @@ class KeywordQueryEventListener(EventListener):
                     icon='images/tmux.png',
                     name='Create a new tmux session',
                     description='No active tmux sessions found',
-                    on_enter=RunScriptAction("gnome-terminal --tab -e \"tmux\"", None)
+                    on_enter=RunScriptAction(terminal_binary + ' ' + console_parameters_new, None)
                 )
             )
 
